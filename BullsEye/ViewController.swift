3733bd5d41a5421001d2ab1eea91380758fcb77a
IssacCZ <sheldowkay@icloud.com>
2015-10-25 21:17:17 +0800

    Add some UI

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 50
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +23,17 @@ class ViewController: UIViewController {
     
     
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World", message: "Ah ha?", preferredStyle: .Alert)
-        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
+        let message = "The value of the slider is: \(currentValue)"
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
 }
 
