90fd3c08f04fcd82a8a089b7c943c1ab3f805a5a
IssacCZ <sheldowkay@icloud.com>
2015-10-26 14:51:51 +0800

    Add some Outlets
    Link the .storyboard and the .swift by IBAction and IBOutlet

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -7,13 +7,21 @@
 //
 
 import UIKit
+import PhotosUI
+import Photos
 
 class ViewController: UIViewController {
-    var currentValue: Int = 50
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        startNewround()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,17 +31,30 @@ class ViewController: UIViewController {
     
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
         let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
 
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
+        
+        startNewround()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewround() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 }
 
